4ddd83b1539f5804578288df4c684173c5979013
Dinfes <147631935+Dinfes@users.noreply.github.com>
2023-11-08 16:19:34 +0100

    Update OgHardwareMap.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OgHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OgHardwareMap.java
@@ -1,6 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class OgHardwareMap {
+    HardwareMap hdwMap = null;
+
+    public DcMotor motor1 = null;
+    public DcMotor motor2 = null;
+    public DcMotor motor3 = null;
+    public DcMotor motor4 = null;
+
+    public void init(HardwareMap ahwMap){
+        hdwMap = ahwMap;
+
+        motor1 = hdwMap.get(DcMotor.class, "leftFront");
+        motor2 = hdwMap.get(DcMotor.class, "rightFront");
+        motor3 = hdwMap.get(DcMotor.class, "leftBack");
+        motor4 = hdwMap.get(DcMotor.class, "rightBack");
 
 
+    }
 }
