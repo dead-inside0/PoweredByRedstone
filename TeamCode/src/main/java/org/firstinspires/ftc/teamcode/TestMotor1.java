e8006a23523c0f2c243b6cffa5f8b6f91cb8b64b
Ernest Levkovich <ernest@levkovit.ch>
2023-11-15 16:37:21 +0100

    sasfa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotor1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMotor1.java
@@ -1,25 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.MyHardwareMap;
 
-@TeleOp(name="Test motor 1", group="Linear OpMode")
-@Disabled
-public class TestMotor1 extends LinearOpMode {
+@TeleOp(name="Test motor 1")
+public class TestMotor1 extends OpMode {
     MyHardwareMap map = new MyHardwareMap();
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     private DcMotor motor = null;
 
     @Override
-    public void runOpMode() {
+    public void init() {
         motor = map.motor1;
-        motor.setPower(20);
         motor.setDirection((DcMotorSimple.Direction.FORWARD));
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+        motor.setPower(20);
     }
 }
