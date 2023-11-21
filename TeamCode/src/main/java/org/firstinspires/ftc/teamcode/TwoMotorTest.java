4872a270161246aff5d84155844f743e167f21dd
Ernest Levkovich <ernest@levkovit.ch>
2023-11-21 12:05:08 +0100

    Added 2 motor test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TwoMotorTest.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Two-Motor joystick test")
+public class TwoMotorTest extends OpMode {
+
+    private MyHardwareMap hMap = new MyHardwareMap();
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftMotor = null;
+    private DcMotor rightMotor = null;
+
+    @Override
+    public void init() {
+        //Left
+        //leftMotor = hardwareMap.get(DcMotor.class, "motor1");
+        leftMotor = hMap.motor1;
+        //Right
+        //rightMotor = hardwareMap.get(DcMotor.class, "motor2");
+        rightMotor = hMap.motor2;
+        leftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    @Override
+    public void loop() {
+        double forward = -gamepad1.left_stick_y;
+        double rotate = gamepad1.left_stick_x;
+        leftMotor.setPower(forward-rotate);
+        rightMotor.setPower(forward+rotate);
+        telemetry.addData("Joystick X", rotate);
+        telemetry.addData("Joystick Y", forward);
+        telemetry.addData("Runtime", runtime.toString());
+    }
+}
