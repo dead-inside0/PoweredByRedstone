63f8058111e248c7cfad80c20d0e5887815fcce2
Ernest Levkovich <ernest@levkovit.ch>
2023-11-30 09:45:09 +0100

    Cleanup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/FourMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/FourMotorTest.java
@@ -1,6 +1,7 @@
 //note: Motors config works only for the test rig. As the naming refers to the positions of the motor on the rig.
 package org.firstinspires.ftc.teamcode.tests;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,30 +11,18 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.MyHardwareMap;
 
 @TeleOp(name="Four-Motor joystick test")
+@Disabled
 public class FourMotorTest extends OpMode {
+    final private ElapsedTime runtime = new ElapsedTime();
 
-    private MyHardwareMap hMap = new MyHardwareMap(hardwareMap);
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor backleftMotor = null;
-    private DcMotor backrightMotor = null;
-
-    private DcMotor frontleftMotor = null;
-
-    private DcMotor frontrightMotor = null;
+    final private MyHardwareMap hMap = new MyHardwareMap(hardwareMap);
+    final private DcMotor backleftMotor = hMap.motor1;
+    final private DcMotor backrightMotor = hMap.motor2;
+    final private DcMotor frontleftMotor = hMap.motor4;
+    final private DcMotor frontrightMotor = hMap.motor3;
 
     @Override
     public void init() {
-        //Left Back
-        //backleftMotor = hardwareMap.get(DcMotor.class, "motor1");
-        backleftMotor = hMap.motor1;
-        //frontleftMotor = hardwareMap.get(DcMotor.class, "motor4");
-        frontleftMotor = hMap.motor4;
-        //Right Back
-        //backrightMotor = hardwareMap.get(DcMotor.class, "motor2");
-        backrightMotor = hMap.motor2;
-        //frontrightMotor = hardwareMap.get(DcMotor.class, "motor3");
-        frontrightMotor = hMap.motor3;
-
         backleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -47,8 +36,8 @@ public class FourMotorTest extends OpMode {
 
     @Override
     public void loop() {
-        double forward = -gamepad1.left_stick_y;
-        double rotate = gamepad1.left_stick_x;
+        final double forward = -gamepad1.left_stick_y;
+        final double rotate = gamepad1.left_stick_x;
         backleftMotor.setPower(forward-rotate);
         backrightMotor.setPower(forward+rotate);
         frontleftMotor.setPower(forward-rotate);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MecanumWheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MecanumWheelTest.java
@@ -12,24 +12,15 @@ import org.firstinspires.ftc.teamcode.MyHardwareMap;
 
 @TeleOp(name="Mecanum wheel test")
 public class MecanumWheelTest extends OpMode {
-    private MyHardwareMap hMap = new MyHardwareMap(hardwareMap);
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor backleftMotor = null;
-    private DcMotor backrightMotor = null;
-    private DcMotor frontleftMotor = null;
-    private DcMotor frontrightMotor = null;
+    final private ElapsedTime runtime = new ElapsedTime();
 
+    final private MyHardwareMap hMap = new MyHardwareMap(hardwareMap);
+    final private DcMotor backleftMotor = hMap.motor1;
+    final private DcMotor backrightMotor = hMap.motor2;
+    final private DcMotor frontleftMotor = hMap.motor4;
+    final private DcMotor frontrightMotor = hMap.motor3;
     @Override
     public void init() {
-        //backleftMotor = hardwareMap.get(DcMotor.class, "motor1");
-        backleftMotor = hMap.motor1;
-        //frontleftMotor = hardwareMap.get(DcMotor.class, "motor4");
-        frontleftMotor = hMap.motor4;
-        //backrightMotor = hardwareMap.get(DcMotor.class, "motor2");
-        backrightMotor = hMap.motor2;
-        //frontrightMotor = hardwareMap.get(DcMotor.class, "motor3");
-        frontrightMotor = hMap.motor3;
-
         backleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         frontleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -43,9 +34,9 @@ public class MecanumWheelTest extends OpMode {
 
     @Override
     public void loop() {
-        double forward = -gamepad1.left_stick_y;
-        double side = gamepad1.left_stick_x;
-        double rotate = gamepad1.right_stick_x;
+        final double forward = -gamepad1.left_stick_y;
+        final double side = gamepad1.left_stick_x;
+        final double rotate = gamepad1.right_stick_x;
         backleftMotor.setPower(Range.clip(forward-side+rotate,-1.0,1.0));
         backrightMotor.setPower(Range.clip(forward+side-rotate,-1.0,1.0));
         frontleftMotor.setPower(Range.clip(forward+side+rotate,-1.0,1.0));
