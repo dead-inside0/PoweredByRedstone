9846f91d9336be93e5a44537f486208112ee8421
jendahrubec <janhrubec008@gmail.com>
2023-12-22 13:16:48 +0100

    Added new odometry class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MecanumTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MecanumTest.java
@@ -10,26 +10,27 @@ import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.MyHardwareMap;
 
-@TeleOp(name="Mecanum wheel test")
-public class MecanumWheelTest extends OpMode {
+@TeleOp(name="MecanumTest")
+public class MecanumTest extends OpMode {
     final private ElapsedTime runtime = new ElapsedTime();
-
     private MyHardwareMap hMap;
     private DcMotor backleftMotor;
     private DcMotor backrightMotor;
-    private DcMotor frontleftMotor;
     private DcMotor frontrightMotor;
+    private DcMotor frontleftMotor;
     @Override
     public void init() {
+        //get motors from hardware map
         hMap = new MyHardwareMap(hardwareMap);
-        backleftMotor = hMap.motor1;
-        backrightMotor = hMap.motor2;
+        backleftMotor = hMap.motor1; //odo1
+        backrightMotor = hMap.motor2; //odo2
+        frontrightMotor = hMap.motor3; //odo3
         frontleftMotor = hMap.motor4;
-        frontrightMotor = hMap.motor3;
+        //set directions
         backleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         backrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         frontrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
@@ -39,16 +40,18 @@ public class MecanumWheelTest extends OpMode {
 
     @Override
     public void loop() {
+        //get gamepad input
         double forward = -gamepad1.left_stick_y;
         double side = gamepad1.left_stick_x;
         double rotate = gamepad2.right_stick_x;
+        //move robot
         backleftMotor.setPower(Range.clip(forward-side+rotate,-1.0,1.0));
         backrightMotor.setPower(Range.clip(forward+side-rotate,-1.0,1.0));
         frontleftMotor.setPower(Range.clip(forward+side+rotate,-1.0,1.0));
         frontrightMotor.setPower(Range.clip(forward-side-rotate,-1.0,1.0));
+        //output data
         telemetry.addData("Joystick X", side);
         telemetry.addData("Joystick Y", forward);
         telemetry.addData("Runtime", runtime.toString());
     }
 }
-
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/odoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/odoTest.java
@@ -1,25 +1,61 @@
+//note: Motors config works only for the test rig. As the naming refers to the positions of the motor on the rig.
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name="Odometry test")
+import org.checkerframework.checker.units.qual.radians;
+import org.firstinspires.ftc.teamcode.MyHardwareMap;
+
+@TeleOp(name="odoTest")
 public class odoTest extends OpMode {
-
-    public DcMotor odo;
+    final private ElapsedTime runtime = new ElapsedTime();
+    private MyHardwareMap hMap;
+    private DcMotor backleftMotor; //odo left
+    private DcMotor backrightMotor; //odo right
+    private DcMotor frontrightMotor; //odo middle
+    private DcMotor frontleftMotor;
     @Override
     public void init() {
-        odo = hardwareMap.get(DcMotor.class, "odo enc");
-        odo.setDirection(DcMotor.Direction.REVERSE);
-        odo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //get motors from hardware map
+        hMap = new MyHardwareMap(hardwareMap);
+        backleftMotor = hMap.motor1; //odo left
+        backrightMotor = hMap.motor2; //odo right
+        frontrightMotor = hMap.motor3; //odo middle
+        frontleftMotor = hMap.motor4;
+        //set directions
+        backleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        backrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontrightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontleftMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    double[] getCurrentPos(double[] prevPos, double deltaRightOdo, double deltaLeftOdo, double deltaMiddleOdo){
+        final double sideSensorsDistance = 0.3;
+
+        double[] currentPos = {0, 0, 0}; //x, y, angle
+        return currentPos;
     }
 
     @Override
     public void loop() {
-        if(gamepad1.a) {
-            odo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        }
-        telemetry.addData("Encoder", odo.getCurrentPosition());
+        //get gamepad input
+        double forward = -gamepad1.left_stick_y;
+        double side = gamepad1.left_stick_x;
+        double rotate = gamepad2.right_stick_x;
+        //move robot
+        backleftMotor.setPower(Range.clip(forward-side+rotate,-1.0,1.0));
+        backrightMotor.setPower(Range.clip(forward+side-rotate,-1.0,1.0));
+        frontleftMotor.setPower(Range.clip(forward+side+rotate,-1.0,1.0));
+        frontrightMotor.setPower(Range.clip(forward-side-rotate,-1.0,1.0));
     }
 }
