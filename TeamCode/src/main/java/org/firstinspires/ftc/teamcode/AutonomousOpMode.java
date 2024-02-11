663d20b0f7ace449cc913bdeb39ff29e36b853af
Ernest Levkovich <ernest@levkovit.ch>
2024-02-11 17:25:49 +0100

    created autonomous op mode class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class AutonomousOpMode extends LinearOpMode {
+
+    final private ElapsedTime runtime = new ElapsedTime();
+
+
+    final private MyHardwareMap hMap = new MyHardwareMap(hardwareMap);
+    final private DcMotor backLeftMotor = hMap.backLeftMotor;
+    final private DcMotor backRightMotor = hMap.backRightMotor;
+    final private DcMotor frontLeftMotor = hMap.frontLeftMotor;
+    final private DcMotor frontRightMotor = hMap.frontRightMotor;
+
+    /*
+        top left=0
+        bottom left=1
+        top right=2
+        bottom right=3
+     */
+    private int position = 0;
+
+    /*
+        left=0
+        center=1
+        right=2
+     */
+    int pixelLocation() {
+        return 0;
+    };
+
+    public void runOpMode() {
+
+    }
+}
