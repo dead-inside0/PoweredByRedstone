44aedcf94ccf8ef8f06e12397db7e4f3b8fed451
Ernest Levkovich <ernest@levkovit.ch>
2024-02-23 23:03:33 +0200

    tfytfguyhv

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoopmodes/BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoopmodes/BlueLeft.java
@@ -11,10 +11,13 @@ public class BlueLeft extends AutonomousOpMode {
     final double tileLength = 610;
     double[][] path = {
             {},
-            {-tile,tile*1.25,0,0,1,0,0},
-            {-tile*1.25, tile, 0,0,0,-1,1},
-            {-tile*1.25, 0, 0,0,1,1,0},
-            {-tile*2, 0, 0,0,1,1,0},
+            {0, tile*2, 0,0,1,0,0},
+            {-tile, tile*2, 0,0,1,0,0},
+            {-tile*2, tile*2, 0,0,1,0,0},
+            {-tile*3, tile*2, 0,0,1,0,0},
+            {-tile*3.25, tile,0,0,0,-1,1},
+            {-tile*3.25, tile*2,0,1,1,0},
+            {-tile*4,tile*2,0,1,0,0}
     };
 
     @Override
@@ -29,10 +32,10 @@ public class BlueLeft extends AutonomousOpMode {
     @Override
     public double[] getPlacementPosition(int elementLocation) {
         if(elementLocation == 0){
-            return new double[]{-tile,tile*1.25,0,-1,1,0,1};
+            return new double[]{0,tile*1.25,Math.PI,-1,1,0,1};
         }
         else if(elementLocation == 1){
-            return new double[]{0,tile*1.25,-Math.PI * 0.5,-1,1,0,1};
+            return new double[]{0,tile*2,Math.PI * 0.5,-1,1,0,1};
         }
         else if(elementLocation == 2){
             return new double[]{0,tile*1.25,0,-1,1,0,1};
