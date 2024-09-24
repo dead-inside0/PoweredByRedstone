ff0881e0f572606af3803063a24151f5ce3afcc8
Ernest <ernest@Ernests-MacBook-Pro.local>
2024-09-24 08:06:06 +0200

    Install and run script

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!bin/zsh
+
+./gradlew stop
+./gradlew clean installDebug
--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
 
-brew install openjdk openjdk@8 openjdk@11
+brew install openjdk openjdk@8 openjdk@11 openjdk@17
 
-echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/openjdk@8/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/openjdk@11/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >>~/.zshrc
+echo 'export PATH="/usr/local/opt/openjdk@8/bin:$PATH"' >>~/.zshrc
+echo 'export PATH="/usr/local/opt/openjdk@11/bin:$PATH"' >>~/.zshrc
+echo 'export PATH="/usr/local/opt/openjdk@17/bin:$PATH"' >>~/.zshrc
 
 brew install temurin
 
