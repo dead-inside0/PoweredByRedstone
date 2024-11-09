9ec13ca169ff27622abd5dedb2dc7d0986c10926
JanHrubec <janhrubec008@gmail.com>
2024-11-09 13:24:25 +0100

    added odo, driver op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Odometry.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+public class Odometry {
+    //https://chsftcrobotics.weebly.com/uploads/1/2/3/6/123696510/odometry.pdf
+    public static double[] getPositionChange(int deltaContactsRightOdo, int deltaContactsLeftOdo, int deltaContactsMiddleOdo, double prevAngle){
+        //Set known variables
+        final double sideOdosDistance = 300;
+        final double middleOdoDistance = 170;
+        final double wheelCircumference = 60 * Math.PI;
+        final double sensorResolution = 8192;
+
+        //Distance traveled by either odo wheel
+        double rightArcLength = wheelCircumference * (deltaContactsRightOdo / sensorResolution);
+        double leftArcLength = wheelCircumference * (deltaContactsLeftOdo / sensorResolution);
+
+        double strafeArcLength = wheelCircumference * (deltaContactsMiddleOdo / sensorResolution);
+
+        //Rotation change of the car
+        double deltaRotation = (rightArcLength - leftArcLength) / sideOdosDistance;
+
+        double deltaX;
+        double deltaY;
+        if(deltaRotation == 0){
+            deltaX = strafeArcLength;
+            deltaY = (rightArcLength + leftArcLength)/2;
+        }
+        else{
+            //Length of the center arc - average between the two arcs
+            double turningRadius = (sideOdosDistance/2 * (rightArcLength + leftArcLength)) / (rightArcLength - leftArcLength);
+
+            double strafeArcRadius = strafeArcLength/deltaRotation - middleOdoDistance;
+
+            deltaX = turningRadius * (Math.cos(deltaRotation) - 1) + strafeArcRadius * Math.sin(deltaRotation);
+            deltaY = turningRadius * Math.sin(deltaRotation) + strafeArcRadius * (1 - Math.cos(deltaRotation));
+        }
+
+        return new double[]{deltaX, deltaY, deltaRotation};
+    }
+}
